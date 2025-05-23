cb06b41269bf3fe472220bf9bcf0daa15c1b64bb
FelixG105 <felix.gonzalez105@gmail.com>
2025-05-23 13:41:30 -0400

    es lint update

--- app.js
+++ app.js
@@ -29,3 +29,7 @@ app.use((req, res, next) => {
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
+
+module.exports.createClothingItem = (req, res) => {
+  console.log(req.user._id); 
+};
