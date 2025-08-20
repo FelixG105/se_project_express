76d9849b6eedbb396677f177a3bda39cfc644e93
FelixG105 <felix.gonzalez105@gmail.com>
2025-08-20 10:50:57 -0400

    fixing VM pull 1

--- routes/clothingItems.js
+++ routes/clothingItems.js
@@ -1,16 +1,22 @@
 const router = require('express').Router();
 const {
   createItem,
+  getItems,
   deleteItem,
   likeItem,
   unlikeItem,
 } = require('../controllers/clothingItems');
+const authUser = require('../middlewares/auth');
 const {
   createItemValidator,
   itemValidator,
 } = require('../middlewares/validators');
 
+// Read
+router.get('/', getItems);
 
+// Auth middleware
+router.use('/', authUser);
 
 // Create
 router.post('/', createItemValidator, createItem);
--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,10 @@
 const router = require('express').Router();
 const { getCurrentUser, updateProfile } = require('../controllers/users');
 const authUser = require('../middlewares/auth');
-const {
-  headerValidator,
-  updateUserValidator,
-} = require('../middlewares/validators');
+const { updateUserValidator } = require('../middlewares/validators');
 
 // Auth Middleware
-router.use(headerValidator, authUser);
+router.use(authUser);
 
 // Get Current User
 router.get('/me', getCurrentUser);
